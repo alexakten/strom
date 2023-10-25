901229d39c8e40467245d3d6742ae31a7cf1fe8c
Alex <alex.akten@outlook.com>
2023-10-25 12:59:24 +1100

    more quotes added

--- public/quotes.tsx
+++ public/quotes.tsx
@@ -168,10 +168,134 @@ const Quotes = [
   {
     quote: "The only way to deal with an unfree world is to become so absolutely free that your very existence is an act of rebellion.",
     person: "Albert Camus",
+  },
+  {
+    quote: "The journey of a thousand miles begins with one step.",
+    person: "Lao Tzu",
+  },
+  {
+    quote: "He who knows others is wise; he who knows himself is enlightened.",
+    person: "Lao Tzu",
+  },
+  {
+    quote: "The mind is everything. What you think you become.",
+    person: "Buddha",
+  },
+  {
+    quote: "In the sky, there is no distinction of east and west; people create distinctions out of their own minds and then believe them to be true.",
+    person: "Buddha",
+  },
+  {
+    quote: "I do not believe in a fate that falls on men however they act; but I do believe in a fate that falls on them unless they act.",
+    person: "Buddha",
+  },
+  {
+    quote: "To enjoy good health, to bring true happiness to one's family, to bring peace to all, one must first discipline and control one's own mind.",
+    person: "Buddha",
+  },
+  {
+    quote: "The whole secret of existence is to have no fear.",
+    person: "Swami Vivekananda",
+  },
+  {
+    quote: "You will not be punished for your anger, you will be punished by your anger.",
+    person: "Buddha",
+  },
+  {
+    quote: "Better than a thousand hollow words, is one word that brings peace.",
+    person: "Buddha",
+  },
+  {
+    quote: "When you realize nothing is lacking, the whole world belongs to you.",
+    person: "Lao Tzu",
+  },
+  {
+    quote: "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.",
+    person: "Buddha",
+  },
+  {
+    quote: "Act without expectation.",
+    person: "Lao Tzu",
+  },
+  {
+    quote: "When you are content to be simply yourself and don't compare or compete, everyone will respect you.",
+    person: "Lao Tzu",
+  },
+  {
+    quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    person: "Lao Tzu",
+  },
+  {
+    quote: "Silence is a source of great strength.",
+    person: "Lao Tzu",
+  },
+  {
+    quote: "Kindness in words creates confidence. Kindness in thinking creates profoundness. Kindness in giving creates love.",
+    person: "Lao Tzu",
+  },
+  {
+    quote: "Three things cannot be long hidden: the sun, the moon, and the truth.",
+    person: "Buddha",
+  },
+  {
+    quote: "Hatred does not cease by hatred, but only by love; this is the eternal rule.",
+    person: "Buddha",
+  },
+  {
+    quote: "Health is the greatest gift, contentment the greatest wealth, faithfulness the best relationship.",
+    person: "Buddha",
+  },
+  {
+    quote: "All that we are is the result of what we have thought.",
+    person: "Buddha",
+  },
+  {
+    quote: "Your work is to discover your world and then with all your heart give yourself to it.",
+    person: "Buddha",
+  },
+  {
+    quote: "The way is not in the sky. The way is in the heart.",
+    person: "Buddha",
+  },
+  {
+    quote: "We are shaped by our thoughts; we become what we think. When the mind is pure, joy follows like a shadow that never leaves.",
+    person: "Buddha",
+  },
+  {
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+    person: "Confucius",
+  },
+  {
+    quote: "Our greatest glory is not in never falling, but in rising every time we fall.",
+    person: "Confucius",
+  },
+  {
+    quote: "The man who moves a mountain begins by carrying away small stones.",
+    person: "Confucius",
+  },
+  {
+    quote: "To see the right and not to do it is cowardice.",
+    person: "Confucius",
+  },
+  {
+    quote: "He who conquers himself is the mightiest warrior.",
+    person: "Confucius",
+  },
+  {
+    quote: "The more man meditates upon good thoughts, the better will be his world and the world at large.",
+    person: "Confucius",
+  },
+  {
+    quote: "If you are patient in one moment of anger, you will escape a hundred days of sorrow.",
+    person: "Chinese Proverb",
+  },
+  {
+    quote: "The gem cannot be polished without friction, nor man perfected without trials.",
+    person: "Chinese Proverb",
   }
   
-
   
+
   
 ];
 
