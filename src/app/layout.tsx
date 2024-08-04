079465c9e59b5ecab52b5ab9c38a50c850cdd110
Alex <alex.akten@outlook.com>
2024-08-04 20:21:58 +0200

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-// app/layout.tsx
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "./components/ThemeContext";
+import Head from "next/head";
 
 export const metadata = {
   title: "Strōm",
-  description: "Minimalistic writing.",
+  description: "Write with zero distractions.",
 };
 
 export default function RootLayout({
@@ -15,6 +15,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content="/thumbnail.webp" />
+        <meta property="og:url" content="https://use-strom.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content="/thumbnail.webp" />
+      </Head>
       <ThemeProvider>
         <body>
           {children}
